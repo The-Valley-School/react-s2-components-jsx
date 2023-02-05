18980095803670389cd3d2ff55835a1513ab1f16
Fran Linde Blazquez <franlindebl@gmail.com>
2023-02-05 12:00:54 +0100

    Started JSX introduction

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 
 function App() {
+  const name = "Fran";
+  const user = {
+    name: "Fran",
+    address: {
+      street: "Calle de Prueba, 25",
+      city: "Madrid"
+    }
+  }
+
   return (
     <div className="App">
       <h1>Este es el componente App</h1>
+      <p className='hi'>Hola {name}</p>
+      <p>Tu ciudad es {user.address.city}</p>
+      <p>Dos más dos son: {2 + 2}</p>
     </div>
   );
 }
