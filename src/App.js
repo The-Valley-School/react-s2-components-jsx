50efc0ef80bcd434a43e2987691298112b02784b
Fran Linde Blazquez <franlindebl@gmail.com>
2023-02-05 18:08:37 +0100

    Added JSX samples

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 
+function toUpperCase(text) {
+  return text.toUpperCase() + ", ESPAÑA";
+}
+
 function App() {
   const name = "Fran";
   const user = {
@@ -9,13 +13,35 @@ function App() {
       city: "Madrid"
     }
   }
+  const motorcycles = ["Honda", "Yamaha", "BMW", "Ducati"];
+  const cars = ["Volkswagen", "Nissan", "Mazda", "Peugeot"];
+  const userIsLogged = true;
 
   return (
     <div className="App">
-      <h1>Este es el componente App</h1>
+      <h2>Acceso a variables y funciones</h2>
       <p className='hi'>Hola {name}</p>
+      <p>La dirección es: {toUpperCase(user.address.street)}</p>
       <p>Tu ciudad es {user.address.city}</p>
       <p>Dos más dos son: {2 + 2}</p>
+      <p>La moto en segunda posición es {motorcycles[1]}</p>
+
+      <h2>Arrays:</h2>
+      <p>Motos:</p>
+      <ul>
+        {motorcycles.map((motorcycle) => {
+          return <li>{motorcycle}</li>
+        })}
+      </ul>
+      <p>Coches:</p>
+      <ol>
+        {cars.map(car => <li>{car}</li>)}
+      </ol>
+
+      <h2>Condicionales</h2>
+      {userIsLogged ? <p>El usuario está logado.</p> : <p>No está logado.</p>}
+      <p>El usuario {userIsLogged ? "SI" : "NO"} está logado</p>
+
     </div>
   );
 }
